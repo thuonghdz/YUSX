fd5531b9407cefd92a56dc28aac0a5fc7cd88011
thuonghdz <cg-buithithuong@haiduong.edu.vn>
2024-11-18 20:44:31 +0700

    Add files via upload

---
+++ setup.ps1
@@ -0,0 +1,1 @@
+$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P
